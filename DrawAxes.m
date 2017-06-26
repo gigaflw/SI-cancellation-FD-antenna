794e2f5dd43d205b113b21510cecbbfc337fda58
hgs1217 <mf20080540@126.com>
2017-06-26 23:05:24 +0800

    fix zero input bug

--- DrawAxes.m
+++ DrawAxes.m
@@ -1,4 +1,9 @@
 function DrawAxes(UIAxes, T, y, A)
     figure('visible','off');
     plot(UIAxes, T, y);
-    set(UIAxes,'XLim', [T(1)-0.1 T(end)+0.1], 'YLim', [-(A*1.1) (A*1.1)]);
+    if (A == 0)
+        set(UIAxes,'XLim', [T(1)-0.1 T(end)+0.1], 'YLim', [-1 1]);
+    else
+        set(UIAxes,'XLim', [T(1)-0.1 T(end)+0.1], 'YLim', [-(A*1.1) (A*1.1)]);
+    end
+    

8330da9aa22501a6ca5345c3339f20d1ae88c334
hgs1217 <mf20080540@126.com>
2017-06-27 21:43:41 +0800

    缩小了频域显示范围
    -缩小了频域显示范围
    -增大了频域图y轴显示
    -调整了时延的单位
    -删除了自干扰时延
    -删除了期望噪声信号

--- DrawFreqAxes.m
+++ DrawFreqAxes.m
@@ -2,14 +2,9 @@ function y = DrawFreqAxes(UIAxes, y1, y2, fRF)
     figure('visible','off');
     freq = length(y1);
     T = 0: 1: freq-1;
-    A1 = max(abs(y1));
-    A2 = max(abs(y2));
-    A = max(A1, A2);
+    Amax = max(max(y1), max(y2));
+    Amin = min(min(y1), min(y2));
     plot(UIAxes, T, y1, T, y2);
-    minIndex = max(1, fRF+1-5000);
-    maxIndex = min(freq-1, fRF+1+5000);
-    if (A == 0)
-        set(UIAxes,'XLim', [T(minIndex) T(maxIndex)], 'YLim', [-1 1]);
-    else
-        set(UIAxes,'XLim', [T(minIndex) T(maxIndex)], 'YLim', [-(A*1.05) (A*1.05)]);
-    end
+    minIndex = max(1, fRF+1-500);
+    maxIndex = min(freq-1, fRF+1+500);
+    set(UIAxes,'XLim', [T(minIndex) T(maxIndex)], 'YLim', [Amin-5 Amax+5]);

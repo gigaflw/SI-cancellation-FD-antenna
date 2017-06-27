7e2795bae664becf882f9b9142ba89eacf8a5a65
zhouben <a_gigantic_flower@yeah.net>
2017-06-27 21:19:32 +0800

    better algorithm

--- SIC.m
+++ SIC.m
@@ -7,48 +7,48 @@ function recovered = SIC(t, received, reference, E1, fc1, E2, fc2, lowerDelay)
     lower = E2 * exp(1i * (tau * fc2 * t + reference));
     
     % band pass filter
-    fft_upper = bandPassFilter(fft(upper), (fc1+1) * 0.95, (fc2+1) * 1.05);
-    fft_lower = bandPassFilter(fft(lower), (fc1+1) * 0.95, (fc2+1) * 1.05);
-    fft_upper(abs(fft_upper) < max(abs(fft_upper)) * 0.02) = 0;
-    fft_lower(abs(fft_lower) < max(abs(fft_lower)) * 0.02) = 0;
-%     fft_upper = removeNonPeaks(fft_upper, 0.02);
-%     fft_lower = removeNonPeaks(fft_lower, 0.02);
+    fft_upper = bandPassFilter(fft(upper), (fc1+1)-1 , (fc2+1)+1);
+    fft_lower = bandPassFilter(fft(lower), (fc1+1)-1 , (fc2+1)+1);
+
+    fft_upper = removeNonPeaks(fft_upper, 2);
+    fft_lower = removeNonPeaks(fft_lower, 2);
     
+%     fft_upper(abs(fft_upper) > 0)
+%     fft_lower(abs(fft_lower) > 0)
     % recover
     upper_recover = recoverBySSBC(fft_upper);
     lower_recover = recoverBySSBC(fft_lower);
     
-%     shiftN = lowerDelay / T;
-%     lower_recover = circshift(lower_recover, [0, shiftN]);
+    shiftN = floor(lowerDelay / T);
+    lower_recover = [lower_recover(end-shiftN+1:end), lower_recover(1:end-shiftN)];
+
     recovered = upper_recover + lower_recover;
-% 
-%     % plotting
-%     cla;
-%     subplot(2,2,1);
-% 	hold on;
-% 	plot(abs(fft_upper / length(fft_upper)));
-% 	plot(abs(fft_lower / length(fft_lower)));
-% 	title('E_{upper} & E_{lower} (frequency domain)');
-% 
-% 	subplot(2,2,2);
-% 	hold on;
-% 	rcef = timeToLogFreq(received);
-% 	rcof = timeToLogFreq(recovered);
-%     % fRF = find(rcef == max(rcef));
-%     % rcef = rcef(fRF - 100 : fRF + 100);
-%     % rcof = rcof(fRF - 100 : fRF + 100);
-% 	plot(rcef);
-% 	plot(rcof);
-%     % axis([fRF-100, fRF+100, -inf, +inf]);
-% 	title('Signal interested (time domain)');
-% 
-% 	subplot(2,2,3);
-% 	plot(upper_recover);
-% 	title('Recovered E_{upper}');
-% 
-% 	subplot(2,2,4);
-% 	plot(lower_recover);
-% 	title('Recovered E_{lower}');
+
+    % plotting
+    cla;
+    subplot(2,2,1);
+	hold on;
+  	plot(abs(fft_upper / length(fft_upper)));
+  	plot(abs(fft_lower / length(fft_lower)));
+%     plot(fft_upper);
+%     plot(fft_lower);
+	title('E_{upper} & E_{lower} (frequency domain)');
+
+	subplot(2,2,2);
+	hold on;
+	rcef = timeToLogFreq(received);
+	rcof = timeToLogFreq(recovered);
+	plot(rcef);
+	plot(rcof);
+	title('Signal interested (time domain)');
+
+	subplot(2,2,3);
+	plot(upper_recover);
+	title('Recovered E_{upper}');
+
+	subplot(2,2,4);
+	plot(lower_recover + upper_recover);
+	title('Recovered E_{lower}');
 end
 
 function y = recoverBySSBC(x)
@@ -63,11 +63,11 @@ function y = bandPassFilter(x, lower ,upper)
     y(upper:end) = 0;
 end
 
-function y = removeNonPeaks(x, threshold)
+function y = removeNonPeaks(x, npeaks)
     [peaks, locs] = findpeaks(abs(x));
+    [~, index] = sort(peaks, 'descend');
     y = zeros(1, length(x));
-    y(locs) = x(locs);
-    
+    y(locs(index(1:npeaks))) = x(locs(index(1:npeaks)));
     % remove peaks which are too small
-    y(abs(y) < max(peaks) * threshold) = 0;
+    % y(abs(y) < max(peaks) * threshold) = 0;
 end
--- SICScript.m
+++ SICScript.m
@@ -1,16 +1,16 @@
 tau = 2*pi;
 fRF = 4000;
 t = 0:0.00001:1;   % must ends at 1?
-Vs = 0.2;
-Vr = 0.4;
-% deltaT = 0.001;   % used to delay interference signal
-deltaT = 0;
+Vs = 0.04;
+Vr = 0.8;
+deltaT = 0.0001;   % used to delay interference signal
 
 s = Vs * cos(tau * fRF * t);
-% r = Vr * cos(tau * fRF * t);
-% i = Vr * cos(tau * fRF * (t - deltaT));
-r = wgn(1, length(t), -10);
-i = wgn(1, length((t - deltaT)), -10);
+r = Vr * cos(tau * fRF * t);
+i = Vr * cos(tau * fRF * (t - deltaT));
+% r = Vr * wgn(1, length(t), -10);
+% r = 1;
+% i = r;
 
 fc1 = 20000;
 fc2 = 30000;
